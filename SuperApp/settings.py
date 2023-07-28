e323b5357b3bb3ac00bae550d169a767afe6fc0c
azharfa99 <azharfaturohman29@gmail.com>
2023-07-29 00:48:11 +0700

    Memperbaiki settingan mysql

--- SuperApp/settings.py
+++ SuperApp/settings.py
@@ -85,6 +85,11 @@ DATABASES = {
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+                    'charset': 'utf8mb4',
+                    "autocommit": True,
+                }
     }
 }
 
